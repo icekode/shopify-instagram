0dbbe3ecfeb7df4aaa8c4ed08ddcc0a161b9cfba
root <root@shopify-test-instance.us-central1-a.c.icekodeorg.internal>
2019-11-26 17:26:07 +0000

    Configured server.js to support https in koa along with shopify code.

--- server.js
+++ server.js
@@ -3,80 +3,86 @@
 // 10-26-19
 // (C) 2019 IceKode Creations
 
-'use strict';
-
-const fs = require('fs');
-
+require('isomorphic-fetch');
+const dotenv = require('dotenv');
+const Koa = require('koa');
+const next = require('next');
+const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
+const { verifyRequest } = require('@shopify/koa-shopify-auth');
+const session = require('koa-session');
+const https = require('https');
+const fs = require('fs')
 const path = require('path');
 
-const https = require('https');
+dotenv.config();
 
-const Koa = require('koa');
+const port = parseInt(process.env.PORT, 10) || 443;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-const server = new Koa();
+const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY } = process.env;
 
-// add main routes
+// APP
+app.prepare().then(() => {
+    // Routing Middleware
+    const server = new Koa();
+    server.use(session(server));
+    server.keys = [SHOPIFY_API_SECRET_KEY];
+    server.use(
+        createShopifyAuth({
+            apiKey: SHOPIFY_API_KEY,
+            secret: SHOPIFY_API_SECRET_KEY,
+            scopes: ['read_products'],
+            afterAuth(ctx) {
+                const { shop, accessToken } = ctx.session;
+                     ctx.cookies.set('shopOrigin', shop, { httpOnly: false });
+                ctx.redirect('/');
+            },
+        }),
+    );
 
-// the following routes are for the authorisation challenges
+    server.use(verifyRequest());
+    server.use(async (ctx) => {
+        await handle(ctx.req, ctx.res);
+        ctx.respond = false;
+        ctx.res.statusCode = 200;
+        return
+    });
 
-// ... we'll come back to this shortly
+    //Setting up Port
+    const config = {
+        domain: 'shopifydev.icekode.com', // your domain
+        https: {
+            port: 443, // any port that is open and not already used on your server
+            options: {
+                key: fs.readFileSync(path.resolve(process.cwd(), '../../../etc/letsencrypt/live/shopifydev.icekode.com/privkey.pem'), 'utf8').toString(),
+                cert: fs.readFileSync(path.resolve(process.cwd(), '../../../etc/letsencrypt/live/shopifydev.icekode.com/fullchain.pem'), 'utf8').toString(),
+            },
+        },
+    };
+    
+    const serverCallback = server.callback();
 
-const router = require('./router.js');
+    try {
+        const httpsServer = https.createServer(config.https.options, serverCallback);
+        httpsServer
+            .listen(config.https.port, function(err) {
+                if (!!err) {
+                    console.error('HTTPS server FAIL: ', err, (err && err.stack));
+                }
+                else {
+                    console.log(`HTTPS server OK: https://${config.domain}:${config.https.port}`);
+                }
+            });
+    } catch (ex) {
+        console.error('Failed to start HTTPS server\n', ex, (ex && ex.stack));
+    }
 
-server
-
-.use(router.routes())
-
-.use(router.allowedMethods());
-
-const config = {
-
-domain: 'shopifydev.icekode.com', // your domain
-
-https: {
-
-port: 443, // any port that is open and not already used on your server
-
-options: {
-
-key: fs.readFileSync(path.resolve(process.cwd(), '../../../etc/letsencrypt/live/shopifydev.icekode.com/privkey.pem'), 'utf8').toString(),
-
-cert: fs.readFileSync(path.resolve(process.cwd(), '../../../etc/letsencrypt/live/shopifydev.icekode.com/fullchain.pem'), 'utf8').toString(),
-
-},
-
-},
-
-};
-
-const serverCallback = server.callback();
-
-try {
-
-const httpsServer = https.createServer(config.https.options, serverCallback);
-
-httpsServer
-
-.listen(config.https.port, function(err) {
-
-if (!!err) {
-
-console.error('HTTPS server FAIL: ', err, (err && err.stack));
-
-}
-
-else {
-
-console.log(`HTTPS server OK: https://${config.domain}:${config.https.port}`);
-
-}
-
+    /*
+    server.listen(port, () => {
+        console.log(`> Ready on http://localhost:${port}`);
+    });
+*/
 });
 
-}
-
-catch (ex) {
-
-console.error('Failed to start HTTPS server\n', ex, (ex && ex.stack));
-
-}
